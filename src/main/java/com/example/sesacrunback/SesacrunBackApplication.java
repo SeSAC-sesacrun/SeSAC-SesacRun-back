08e82fc30bf8dca4e1dd57602a28f883ffd14f60
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-22 10:38:14 +0900

    first commit

---
+++ src/main/java/com/example/sesacrunback/SesacrunBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sesacrunback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacrunBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesacrunBackApplication.class, args);
+    }
+
+}
