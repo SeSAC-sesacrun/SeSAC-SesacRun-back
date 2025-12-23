cc49e90741622078bbf499e709176fe13bb105b8
yu-rim98 <rimmm098@gmail.com>
2025-12-23 10:23:44 +0900

    Feat: 카테고리 enum 대소문자 구별을 위한 정적 팩토리 메서드 추가

--- src/main/java/com/example/sesacrunback/domain/recruitment/post/entity/RecruitmentCategory.java
+++ src/main/java/com/example/sesacrunback/domain/recruitment/post/entity/RecruitmentCategory.java
@@ -1,6 +1,15 @@
 package com.example.sesacrunback.domain.recruitment.post.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum RecruitmentCategory {
     STUDY,    // 스터디
-    PROJECT   // 팀 프로젝트
+    PROJECT;   // 팀 프로젝트
+
+    // JSON 값을 받을 때 해당 메서드를 거침
+    @JsonCreator
+    public static RecruitmentCategory from(String value) {
+        // 대소문자 관계 없이 일치하는 값 반환
+        return RecruitmentCategory.valueOf(value.toUpperCase());
+    }
 }
