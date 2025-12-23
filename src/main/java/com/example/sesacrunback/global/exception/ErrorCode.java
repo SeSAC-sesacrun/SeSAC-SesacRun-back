b31f7f0a49719e535e314a046f6c31a25c42eb4c
yu-rim98 <rimmm098@gmail.com>
2025-12-23 16:04:43 +0900

    Feat: POST_NOT_FOUND 에러 추가

--- src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
+++ src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    // 모집 글
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다");
 
