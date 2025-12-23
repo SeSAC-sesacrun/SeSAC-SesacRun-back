0901214bc32a7f9659cb4e3e1e69e4ddc200f5b7
ghkdquddlf <xhdlapdlxm12@naver.com>
2025-12-23 09:17:24 +0900

    Service, Repository 생성

--- src/main/java/com/example/sesacrunback/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/sesacrunback/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.sesacrunback.domain.user.repository;
 
 import com.example.sesacrunback.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findByEmail(String email);
+
+    boolean existsUserByEmail(String email);
 }
--- src/main/java/com/example/sesacrunback/domain/user/service/UserService.java
+++ src/main/java/com/example/sesacrunback/domain/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.example.sesacrunback.domain.user.service;
 
+import com.example.sesacrunback.domain.user.dto.request.UserCreateReqDto;
+import com.example.sesacrunback.domain.user.dto.response.UserResDto;
+import com.example.sesacrunback.domain.user.repository.UserRepository;
+import com.example.sesacrunback.global.exception.CustomException;
+import com.example.sesacrunback.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResDto signUp(UserCreateReqDto createReqDto) {
+        //사용자 중복된 이메일 검증
+        if(!userRepository.existsUserByEmail(createReqDto.getEmail())){
+            throw new CustomException(ErrorCode.)
+        }
+
+
+    }
 }
