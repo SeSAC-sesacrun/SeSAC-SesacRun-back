6c1cbc81f7c207256496a670a630b611e10fed43
yu-rim98 <rimmm098@gmail.com>
2025-12-23 17:55:20 +0900

    Feat: POST_NOT_OWNER, POST_INVALID_TOTAL_MEMBERS 에러 추가

--- src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
+++ src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
 
     // 모집 글
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
+    POST_NOT_OWNER(HttpStatus.FORBIDDEN, "해당 게시글의 작성자가 아닙니다."),
+    POST_INVALID_TOTAL_MEMBERS(HttpStatus.BAD_REQUEST, "모집 인원은 현재 참여 인원보다 적게 설정할 수 없습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다");
 
