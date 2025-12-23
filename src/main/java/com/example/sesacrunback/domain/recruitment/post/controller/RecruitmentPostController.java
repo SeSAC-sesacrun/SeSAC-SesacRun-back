8aebd9c43001bac8f0e5006e3f043f234ea43dda
yu-rim98 <rimmm098@gmail.com>
2025-12-23 20:30:35 +0900

    Refactor: api path 수정

--- src/main/java/com/example/sesacrunback/domain/recruitment/post/controller/RecruitmentPostController.java
+++ src/main/java/com/example/sesacrunback/domain/recruitment/post/controller/RecruitmentPostController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/community/posts")
+@RequestMapping("/api/recruitments/posts")
 @RequiredArgsConstructor
 public class RecruitmentPostController {
 
