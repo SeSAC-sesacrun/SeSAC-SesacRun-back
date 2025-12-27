f36fdb4ccb200ffeca7b3a0f7da203eed138a3eb
yu-rim98 <rimmm098@gmail.com>
2025-12-27 16:13:03 +0900

    Feat: 기존 엔티티에 필드 추가 및 수정

--- src/main/java/com/example/sesacrunback/domain/chat/chat/entity/Chat.java
+++ src/main/java/com/example/sesacrunback/domain/chat/chat/entity/Chat.java
@@ -1,13 +1,17 @@
 package com.example.sesacrunback.domain.chat.chat.entity;
 
-import com.example.sesacrunback.domain.chat.message.entity.ChatMessage;
+import com.example.sesacrunback.domain.chat.participant.entity.ChatParticipant;
+import com.example.sesacrunback.domain.recruitment.post.entity.RecruitmentPost;
 import com.example.sesacrunback.global.common.entity.BaseCreateEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -25,9 +29,13 @@ public class Chat extends BaseCreateEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // PK
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private RecruitmentPost post; // 모집글
+
     @Column(nullable = false)
     private String name; // 채팅방 이름
 
     @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ChatMessage> chatMessages = new ArrayList<>(); // 채팅 메시지 목록
+    private List<ChatParticipant> participants = new ArrayList<>();
 }
--- src/main/java/com/example/sesacrunback/domain/chat/message/entity/ChatMessage.java
+++ src/main/java/com/example/sesacrunback/domain/chat/message/entity/ChatMessage.java
@@ -5,8 +5,7 @@ import com.example.sesacrunback.domain.user.entity.User;
 import com.example.sesacrunback.global.common.entity.BaseCreateEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,15 +29,11 @@ public class ChatMessage extends BaseCreateEntity {
     @Column(nullable = false)
     private String content; // 메시지 내용
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private MessageType type; // 메시지 타입 (일반, 시스템)
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_id", nullable = false)
     private Chat chat; // 소속된 채팅방
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id", nullable = false)
     private User sender; // 발신자
 }
