5f1a900f6a12c29c5e1fe8a66dd81543744ed104
yu-rim98 <rimmm098@gmail.com>
2025-12-27 21:06:45 +0900

    Remove: 사용되지 않는 파일 삭제

--- src/main/java/com/example/sesacrunback/domain/chat/message/controller/ExController.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.chat.message.controller;
-
-public class ExController {
-
-}
--- src/main/java/com/example/sesacrunback/domain/chat/message/service/ExService.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.chat.message.service;
-
-public class ExService {
-
-}
