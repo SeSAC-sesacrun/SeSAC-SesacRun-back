3cc3720e19b28370dfe1f27332d2d7d735a4bc0b
sukyoung14 <ssukyoung14@gmail.com>
2025-12-28 23:26:00 +0900

    불필요한 파일 삭제

--- src/main/java/com/example/sesacrunback/domain/orderItem/service/ExService.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.orderItem.service;
-
-public class ExService {
-
-}
--- src/main/java/com/example/sesacrunback/domain/payment/dto/request/ExReqDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.payment.dto.request;
-
-public class ExReqDto {
-
-}
