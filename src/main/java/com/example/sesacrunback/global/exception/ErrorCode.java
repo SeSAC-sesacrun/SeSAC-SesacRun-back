45b979cd23fdd0e2d5ff1b262b2ba4ee195843a2
sukyoung14 <ssukyoung14@gmail.com>
2025-12-28 23:10:45 +0900

    errorcode 추가

--- src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
+++ src/main/java/com/example/sesacrunback/global/exception/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode {
     // User
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
+
     // Course
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다"),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다"),
@@ -31,7 +32,13 @@ public enum ErrorCode {
     // Cart
     CART_ITEM_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 장바구니에 담긴 강의입니다."),
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니에서 해당 항목을 찾을 수 없습니다."),
-    COURSE_ALREADY_PURCHASED(HttpStatus.CONFLICT, "이미 구매한 강의입니다.");
+    // ORDER
+    ORDER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 구매한 강의입니다."),
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문내역이 없습니다."),
+    // PAYMENT
+    PAYMENT_AMOUNT_MISMATCH(HttpStatus.BAD_REQUEST, "주문 금액과 실결제 금액이 일치하지 않습니다."),
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제내역이 없습니다."),
+    PAYMENT_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 결제한 강의입니다.");
 
 
     private final HttpStatus status;
