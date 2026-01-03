ce53feab5dadcf76cf21a66b7c9b81f8d0836a53
ghkdquddlf <xhdlapdlxm12@naver.com>
2026-01-03 17:17:25 +0900

    feat: 내정보 조회 기능 구현

--- src/main/java/com/example/sesacrunback/domain/user/controller/UserController.java
+++ src/main/java/com/example/sesacrunback/domain/user/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.example.sesacrunback.domain.user.controller;
 
+import com.example.sesacrunback.domain.user.dto.response.UserResDto;
+import com.example.sesacrunback.domain.user.service.UserService;
+import com.example.sesacrunback.global.common.dto.ApiResponse;
+import com.example.sesacrunback.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
 
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserResDto>> getMyProfile(
+        @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
 
+        return ResponseEntity.ok(ApiResponse.success(userService.getMyProfile(userDetails.getId())));
+    }
 
 
 
--- src/main/java/com/example/sesacrunback/domain/user/service/UserService.java
+++ src/main/java/com/example/sesacrunback/domain/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.example.sesacrunback.domain.user.service;
 
+import com.example.sesacrunback.domain.user.dto.response.UserResDto;
+import com.example.sesacrunback.domain.user.entity.User;
+import com.example.sesacrunback.domain.user.repository.UserRepository;
+import com.example.sesacrunback.global.exception.CustomException;
+import com.example.sesacrunback.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserResDto getMyProfile(Long id) {
+       User user =  userRepository.findById(id).orElseThrow(
+            ()-> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
+       return UserResDto.from(user);
+    }
 }
