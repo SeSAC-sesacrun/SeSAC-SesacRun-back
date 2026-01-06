ddd78fa86205a612bee5e1522bf18d59929eb4ac
sukyoung14 <ssukyoung14@gmail.com>
2026-01-06 15:10:37 +0900

    파일 정리

--- src/main/java/com/example/sesacrunback/domain/refund/repository/ExRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.refund.repository;
-
-public interface ExRepository {
-
-}
--- src/main/java/com/example/sesacrunback/domain/refund/service/ExService.java
+++
@@ -1,5 +0,0 @@
-package com.example.sesacrunback.domain.refund.service;
-
-public class ExService {
-
-}
