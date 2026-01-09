defa0a13ec4df57b105eb6b4554e46a3b8a2ad2d
yu-rim98 <rimmm098@gmail.com>
2026-01-09 10:14:21 +0900

    Fix: 모집 삭제 수정

--- src/main/java/com/example/sesacrunback/domain/chat/message/repository/ChatMessageRepository.java
+++ src/main/java/com/example/sesacrunback/domain/chat/message/repository/ChatMessageRepository.java
@@ -4,11 +4,23 @@ import com.example.sesacrunback.domain.chat.message.entity.ChatMessage;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     Slice<ChatMessage> findAllByChatId(Long chatId, Pageable pageable);
+
+    @Modifying
+    @Query("""
+            delete from ChatMessage cm
+            where cm.chat.id in (
+                select c.id from Chat c where c.post.id = :postId
+            )
+        """)
+    void deleteByPostId(@Param("postId") Long postId);
 
 }
